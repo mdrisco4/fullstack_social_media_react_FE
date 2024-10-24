df8fd0c65e7fdc9fd321b5199425ce2dd212e36d
michaeldriscoll <michaelndriscoll81@gmail.com>
2024-10-23 21:47:46 -0700

    set up some routing for the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Outlet, Navigate, Route, Routes, useLocation, replace } from "react-router-dom";
 
+function Layout () {
+  const user = null;
+  const location = useLocation()
+
+  return user?.token ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{from: location}} replace />
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-full min-h-[100vh]'>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile/:id>' element={<Profile />} />
+        </Route>
+
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+      </Routes>
     </div>
   );
 }

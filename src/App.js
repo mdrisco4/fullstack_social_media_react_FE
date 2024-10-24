4c443681cf3c9f96a8433ce1b25022360af7d936
michaeldriscoll <michaelndriscoll81@gmail.com>
2024-10-23 21:58:46 -0700

    created pages and set up how frontend will be displayed and navigated and started setting up redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Outlet, Navigate, Route, Routes, useLocation, replace } from "react-router-dom";
+import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Home, Login, Profile, Register, ResetPassword } from "./pages";
 
 function Layout () {
   const user = null;

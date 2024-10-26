bd2adcb0441b27e799d25ba7a557a0a6f1d8f8d2
michaeldriscoll <michaelndriscoll81@gmail.com>
2024-10-26 15:05:03 -0700

    fixed a small bit of styling on the header bar and added a Logout button.

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -9,6 +9,7 @@ import { SetTheme } from '../redux/theme';
 import { fetchPosts } from '../utils';
 import { BsMoon, BsSunFill } from 'react-icons/bs';
 import { IoMdNotificationsOutline } from "react-icons/io";
+import { Logout } from '../redux/userSlice';
 
 
 
@@ -37,7 +38,7 @@ const TopBar = () => {
   return (
     <div className='topbar w-full flex items-center justify-between py-3 md:py-6 px-4 bg-primary'>
         <Link to='/' className='flex gap-2 items-center'>
-            <div>
+            <div className='p-1 md:p-2 bg-[#065ad8] rounded text-white'>
                 <TbSocial />
             </div>
             <span className='text-x1 md:text-2xl text-[#065ad8] font-semibold'>
@@ -49,14 +50,14 @@ const TopBar = () => {
             onSubmit={handleSubmit(handleSearch)}
         >
             <TextInput 
-                placerholder='Search...'
+                placeholder='Search...'
                 styles='w-[18rem] lg:w[38rem] rounded-1-full py-3'
                 register={register("search")}
             />
             <CustomButton 
                 title='Search'
                 type='submit'
-                containerStyles='bg-[#0444a4] text-white px-6 py-2.5 mt-2 rounded-r-full'
+                containerStyles='bg-[#0444a4] text-white px-6 py-2.5 rounded-r-full'
             />
         </form>
         {/* ICONS */}
@@ -66,6 +67,13 @@ const TopBar = () => {
             </button>
             <div className='hidden lg:flex'>
                 <IoMdNotificationsOutline />
+            </div>
+            <div>
+                <CustomButton 
+                    onClick={() => dispatch(Logout())}
+                    title='logout'
+                    containerStyles='text-sm text-ascent-1 px-4 md:px-6 py-1 md:py-2 border border-[#666] rounded-full'
+                />
             </div>
         </div>
     </div>

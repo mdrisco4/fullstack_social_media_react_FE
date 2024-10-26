f6da2f1f2f84d65e73c17b5ba75c27f376b6e21b
michaeldriscoll <michaelndriscoll81@gmail.com>
2024-10-25 18:09:49 -0700

    fixed a typo in Login page styling.  Didn`t look correct.  Continuing.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     })
   return (
     <div className='bg-bgColor w-full h-[100vh] flex items-center justify-center p-6'>
-        <div className='w-full md:2-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl'>
+        <div className='w-full md:w-2/3 h-fit lg:h-full 2xl:h-5/6 py-8 lg:py-0 flex bg-primary rounded-xl overflow-hidden shadow-xl'>
             {/* LEFT */}
             <div className='w-full lg:w1/2 h-full p-10 2xl:px-20 flex flex-col justify-center'>
                 <div className='w-full flex gap-2 items-center mb-6'>

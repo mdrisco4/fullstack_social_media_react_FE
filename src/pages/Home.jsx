901e8baee5a94cb8987f447618a85f0cf07cda78
michaeldriscoll <michaelndriscoll81@gmail.com>
2024-10-28 16:31:54 -0700

    setting up the right part of the Home Page and using test data to confrim it is working properly.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { 
     ProfileCard,
     TopBar,
     FriendsCard 
 } from "../components";
+import { friends } from "../assets/data";
 
 const Home = () => {
     const {user} = useSelector((state) => state.user)
-
+    const [friendRequest, setFriendRequest] = useState(friends);
+     const [suggestedFriends, setSuggestedFriends] = useState(friends);
 
     return (
         <div className="home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor lg:rounded-lg h-screen overflow-hidden">
@@ -20,11 +22,20 @@ const Home = () => {
                     <FriendsCard friends={user?.friends} />
                 </div>
                 {/* CENTER */}
-                <div>
+                <div className="flex-1 h-full bg-primary px-4 flex flex-col gap-6 overflow-y-auto">
 
                 </div>
                 {/* RIGHT */}
-                <div>
+                <div className="hidden w-1/4 h-full lg:flex flex-col gap-8 overflow-y-auto">
+                    {/* FRIEND REQUESTS */}
+                    <div className="w-full bg-primary shadow-sm rounded-lg px-6 py-5">
+                        <div className="flex items-center justify-between text-xl text-ascent-1 pb-2 border-b border-[#66666645]">
+                            <span>Friend Requests</span>
+                            <span>{friendRequest?.length}</span>
+                        </div>
+                    </div>
+                    {/* SUGGESTED FRIENDS */}
+                    <div></div>
 
                 </div>
             </div>

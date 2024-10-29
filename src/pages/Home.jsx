d9aa74e036c6f319b1048b8d4df0eb940ad36a2c
michaeldriscoll <michaelndriscoll81@gmail.com>
2024-10-28 17:21:51 -0700

    set up suggestions part of the page, set up new data to see it.  GTG.  need to set up handleFriendRequest function.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { ProfileCard, TopBar, FriendsCard, CustomButton } from "../components";
-import { friends, requests } from "../assets/data";
+import { friends, requests, suggestions } from "../assets/data";
 import { Link } from "react-router-dom";
 import { NoProfile } from "../assets";
 import { apiRequest } from "../utils";
+import { BsPersonFillAdd } from "react-icons/bs";
 
 const Home = () => {
   const { user } = useSelector((state) => state.user);
   const [friendRequest, setFriendRequest] = useState(requests);
-  const [suggestedFriends, setSuggestedFriends] = useState(friendRequest);
+  const [suggestedFriends, setSuggestedFriends] = useState(suggestions);
 
   const acceptFriendRequest = async (id, status) => {
     try {
@@ -87,7 +88,47 @@ const Home = () => {
             </div>
           </div>
           {/* SUGGESTED FRIENDS */}
-          <div></div>
+          <div className="w-full bg-primary shadow-sm rounded-lg px-5 py-5">
+            <div className="flex items-center justify-between text-lg text-ascent-1 border-b border-[#66666645]">
+              <span>Friend Suggestions</span>
+            </div>
+            <div className="w-full flex flex-col gap-4 pt-4">
+              {suggestedFriends?.map((friend, index) => (
+                <div
+                  className="flex items-center justify-between"
+                  key={index + friend?._id}
+                >
+                  <Link
+                    to={"/profile/" + friend?._id}
+                    key={friend?._id}
+                    className="w-full flex gap-4 items-center cursor-pointer"
+                  >
+                    <img
+                      src={friend?.profileUrl ?? NoProfile}
+                      alt={friend?.firstName}
+                      className="w-10 h-10 object-cover rounded-full"
+                    />
+                    <div className="flex-1 ">
+                      <p className="text-base font-medium text-ascent-1">
+                        {friend?.firstName} {friend?.lastName}
+                      </p>
+                      <span className="text-sm text-ascent-2">
+                        {friend?.profession ?? "No Profession"}
+                      </span>
+                    </div>
+                  </Link>
+                  <div className="flex gap-1">
+                    <button
+                      className="bg-[#0444a430] text-sm text-white p-1 rounded"
+                    //   onClick={() => handleFriendRequest(friend?._id)}
+                    >
+                      <BsPersonFillAdd size={20} className="text-[#0f52b6]" />
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
